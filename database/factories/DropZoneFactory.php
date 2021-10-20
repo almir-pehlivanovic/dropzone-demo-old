9d6330f3927086c3816a7e99a27484fed9ed1e29
almir-pehlivanovic <pehlivanovicalmir1@gmail.com>
2021-10-20 17:49:31 +0200

    Setting up database (Model, Migrations, Seeders)

---
+++ database/factories/DropZoneFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\DropZone;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DropZoneFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = DropZone::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
+            'body' => $this->faker->text($maxNbChars = 200),
+            'images' => '',
+        ];
+    }
+}
---
+++ database/migrations/2021_10_20_153918_create_drop_zones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDropZonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drop_zones', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->text('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drop_zones');
+    }
+}
